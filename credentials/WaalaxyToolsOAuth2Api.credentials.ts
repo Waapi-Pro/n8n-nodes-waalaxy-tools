60c51d2c9c4426a49bce30c6e8ebea9b0d7a7e39
Simon Moulin <simon.moulin@waapi.fr>
2026-03-06 12:49:03 +0100

    feat: add credentials and node

---
+++ credentials/WaalaxyToolsOAuth2Api.credentials.ts
@@ -0,0 +1,57 @@
+import type { Icon, ICredentialType, INodeProperties } from 'n8n-workflow';
+
+export class WaalaxyToolsOAuth2Api implements ICredentialType {
+	name = 'waalaxyToolsOAuth2Api';
+
+	extends = ['oAuth2Api'];
+
+	displayName = 'Waalaxy Tools OAuth2 API';
+
+	icon: Icon = {
+		light: 'file:../icons/logo-api-gradient.svg',
+		dark: 'file:../icons/logo-api-gradient-dark.svg',
+	};
+
+	documentationUrl = 'https://google.com';
+
+	properties: INodeProperties[] = [
+		{
+			displayName: 'Grant Type',
+			name: 'grantType',
+			type: 'hidden',
+			default: 'authorizationCode',
+		},
+		{
+			displayName: 'Authorization URL',
+			name: 'authUrl',
+			type: 'hidden',
+			default: 'https://waalaxy-tools-api-01b7e745bb4d.herokuapp.com/api/auth/authorize',
+			required: true,
+		},
+		{
+			displayName: 'Access Token URL',
+			name: 'accessTokenUrl',
+			type: 'hidden',
+			default: 'https://waalaxy-tools-api-01b7e745bb4d.herokuapp.com/api/auth/token',
+			required: true,
+		},
+		{
+			displayName: 'Scope',
+			name: 'scope',
+			type: 'hidden',
+			default: '',
+		},
+		{
+			displayName: 'Auth URI Query Parameters',
+			name: 'authQueryParameters',
+			type: 'hidden',
+			default: '',
+		},
+		{
+			displayName: 'Authentication',
+			name: 'authentication',
+			type: 'hidden',
+			default: 'header',
+		},
+	];
+}
---
+++ nodes/WaalaxyTools/WaalaxyTools.node.ts
@@ -0,0 +1,108 @@
+import {
+	type IExecuteFunctions,
+	type INodeExecutionData,
+	type INodeType,
+	type INodeTypeDescription,
+} from 'n8n-workflow';
+
+const BASE_URL = 'https://waalaxy-tools-api-01b7e745bb4d.herokuapp.com';
+
+export class WaalaxyTools implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'Waalaxy Tools',
+		name: 'waalaxyTools',
+		icon: {
+			light: 'file:../../icons/logo-api-gradient.svg',
+			dark: 'file:../../icons/logo-api-gradient-dark.svg',
+		},
+		group: ['input'],
+		version: 1,
+		subtitle: '={{$parameter["operation"]}}',
+		description: 'Consume tools from the Waalaxy API',
+		defaults: {
+			name: 'Waalaxy Tools',
+		},
+		usableAsTool: true,
+		inputs: ['main'],
+		outputs: ['main'],
+		credentials: [
+			{
+				name: 'waalaxyToolsOAuth2Api',
+				required: true,
+			},
+		],
+		properties: [
+			{
+				displayName: 'Operation',
+				name: 'operation',
+				type: 'options',
+				noDataExpression: true,
+				options: [
+					{
+						name: 'Find LinkedIn URL',
+						value: 'findLinkedinUrl',
+						action: 'Find a linked in profile url',
+						description: 'Find a linked in profile URL by first name, last name and company',
+					},
+				],
+				default: 'findLinkedinUrl',
+			},
+			{
+				displayName: 'First Name',
+				name: 'firstName',
+				type: 'string',
+				required: true,
+				default: '',
+				description: 'The first name of the person to search for',
+			},
+			{
+				displayName: 'Last Name',
+				name: 'lastName',
+				type: 'string',
+				required: true,
+				default: '',
+				description: 'The last name of the person to search for',
+			},
+			{
+				displayName: 'Company',
+				name: 'company',
+				type: 'string',
+				required: true,
+				default: '',
+				description: 'The company of the person to search for',
+			},
+		],
+	};
+
+	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		const items = this.getInputData();
+		const returnData: INodeExecutionData[] = [];
+		const credentials = await this.getCredentials('waalaxyToolsOAuth2Api');
+		const { access_token: accessToken } = credentials.oauthTokenData as { access_token: string };
+
+		for (let i = 0; i < items.length; i++) {
+			const firstName = this.getNodeParameter('firstName', i) as string;
+			const lastName = this.getNodeParameter('lastName', i) as string;
+			const company = this.getNodeParameter('company', i) as string;
+
+			const response = await this.helpers.httpRequest({
+				method: 'GET',
+				url: `${BASE_URL}/api/linkedin-url`,
+				qs: { firstName, lastName, company },
+				headers: {
+					Authorization: `Bearer ${accessToken}`,
+				},
+				json: true,
+			});
+
+			const json = (
+				typeof response === 'object' && response !== null ? response : { result: response }
+			) as {
+				[key: string]: never;
+			};
+			returnData.push({ json });
+		}
+
+		return [returnData];
+	}
+}
